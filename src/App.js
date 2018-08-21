fd34a12a7119c2fe62eb806049744c2738ad87ef
Shurui Liu <shurui91@gmail.com>
2018-08-21 10:13:02 -0700

    start to add states to different persons

--- src/App.js
+++ src/App.js
@@ -3,12 +3,34 @@ import "./App.css";
 import Person from "./Person/Person";
 
 class App extends Component {
+	state = {
+		persons: [
+			{ name: "max", age: 28 },
+			{ name: "Manu", age: 29 },
+			{ name: "Stephanie", age: 26 }
+		]
+	};
+
 	render() {
 		return (
 			<div className="App">
 				<h1>Hi, I am a react App!</h1>
 				<p>This is really working!</p>
-				<Person />
+				<button>Switch Name</button>
+				<Person
+					name={this.state.persons[0].name}
+					age={this.state.persons[0].age}
+				/>
+				<Person
+					name={this.state.persons[1].name}
+					age={this.state.persons[1].age}
+				>
+					My hobbies, Racing
+				</Person>
+				<Person
+					name={this.state.persons[2].name}
+					age={this.state.persons[2].age}
+				/>
 			</div>
 		);
 	}
