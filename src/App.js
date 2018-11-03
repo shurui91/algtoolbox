0c2fccf535361c9cf4a2cfcc84dfb3322fc0033d
Shurui Liu <shurui91@gmail.com>
2018-11-03 12:48:28 -0700

    done with section 4

--- src/App.js
+++ src/App.js
@@ -11,24 +11,11 @@ class App extends Component {
 		],
 		otherState: 'some other value',
 		showPersons: false
-	};
+	}
 
-<<<<<<< HEAD
 	nameChangedHandler = (event, id) => {
 		const personIndex = this.state.persons.findIndex(p => {
 			return p.id === id;
-=======
-	/*
-	switchNameHandler = newName => {
-		// console.log('Was clicked!');
-		// DON'T DO THIS: this.state.persons[0].name = 'Maximilian';
-		this.setState({
-			persons: [
-				{ name: newName, age: 28 },
-				{ name: 'Manu', age: 29 },
-				{ name: 'Stephanie', age: 27 }
-			]
->>>>>>> d82264278b23b0d1c77b44444eba6bd77c02cd86
 		});
 
 		const person = {
@@ -43,24 +30,19 @@ class App extends Component {
 		persons[personIndex] = person;
 
 		this.setState({ persons: persons });
-	};
-	*/
+	}
 
-	deletePersonHandler = personIndex => {
+	deletePersonHandler = (personIndex) => {
 		// const persons = this.state.persons.slice();
 		const persons = [...this.state.persons];
 		persons.splice(personIndex, 1);
 		this.setState({ persons: persons });
-	};
-
-	deletePersonHandler = () => {
-		
 	}
 
 	togglePersonsHandler = () => {
 		const doesShow = this.state.showPersons;
 		this.setState({ showPersons: !doesShow });
-	};
+	}
 
 	render() {
 		const style = {
@@ -77,17 +59,12 @@ class App extends Component {
 			persons = (
 				<div>
 					{this.state.persons.map((person, index) => {
-						return (
-							<Person
-								click={() => this.deletePersonHandler(index)}
-								name={person.name}
-								age={person.age}
-								key={person.id}
-								changed={event =>
-									this.nameChangedHandler(event, person.id)
-								}
-							/>
-						);
+						return <Person
+							click={() => this.deletePersonHandler(index)}
+							name={person.name}
+							age={person.age}
+							key={person.id}
+							changed={(event) => this.nameChangedHandler(event, person.id)} />
 					})}
 				</div>
 			);
@@ -97,9 +74,9 @@ class App extends Component {
 			<div className="App">
 				<h1>Hi, I'm a React App</h1>
 				<p>This is really working!</p>
-				<button style={style} onClick={this.togglePersonsHandler}>
-					Toggle Persons
-				</button>
+				<button
+					style={style}
+					onClick={this.togglePersonsHandler}>Toggle Persons</button>
 				{persons}
 			</div>
 		);
