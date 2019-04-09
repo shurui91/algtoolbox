60b1ef478850db7d9e19a66458df82dd1320e6fc
Shurui Liu <shurui91@gmail.com>
2019-04-09 11:37:09 -0700

    lecture 34

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
 	render() {
-		// return (
-		// 	<div className="App">
-		// 		<h1>Hi, I am a React App!</h1>
-		// 	</div>
-		// );
-		return React.createElement('div', { className: 'App' }, React.createElement('h1', null, 'does it work now?'));
+		return (
+			<div className="App">
+				<h1>Hi, I am a React App!</h1>
+				<Person name="Max" age="28" />
+				<Person name="Max2" age="22">
+					child element
+				</Person>
+				<Person name="Max33" age="33" />
+			</div>
+		);
+		// return React.createElement('div', { className: 'App' }, React.createElement('h1', null, 'does it work now?'));
 	}
 }
 
