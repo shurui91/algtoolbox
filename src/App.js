9375fd09c72d2cebedaa7b35470fbfe056c1b0d4
Shurui Liu <shurui91@gmail.com>
2019-04-11 10:22:00 -0700

    lecture 47

--- src/App.js
+++ src/App.js
@@ -22,11 +22,21 @@ class App extends Component {
 	};
 
 	render() {
+		const style = {
+			backgroundColor: 'white',
+			font: 'inherit',
+			border: '1x solid blue',
+			padding: '8px',
+			cursor: 'pointer'
+		};
+
 		return (
 			<div className="App">
 				<h1>Hi, I am a React App!</h1>
 				<p>This is really working!!</p>
-				<button onClick={() => this.switchNameHandler('MaxAgain!!')}>Switch Name</button>
+				<button style={style} onClick={() => this.switchNameHandler('MaxAgain!!')}>
+					Switch Name
+				</button>
 				<Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
 				<Person
 					name={this.state.persons[1].name}
